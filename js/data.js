144cd5f4816da4b1fc662e4911903edf31cb0187
Nadezhda Obidina <nadezhdaobidina@rambler.ru>
2017-12-14 00:09:30 +0300

    убрала закомментированный код

--- js/data.js
+++ js/data.js
@@ -1,34 +1,6 @@
 'use strict';
 
 (function () {
-  // // рандомные числа в диапозоне
-  // var getRandomInt = function (min, max) {
-  //   return Math.floor(Math.random() * (max - min + 1)) + min;
-  // };
-  //
-  // // рандомные числа
-  // var getRandom = function (number) {
-  //   return Math.floor(Math.random() * number);
-  // };
-  //
-  // // массив рандомных особенностей жилья
-  // var getFeatures = function () {
-  //   var AD_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  //   var countOfFeatures = getRandom(AD_FEATURES.length);
-  //   var adRandomFeatures = [];
-  //
-  //   for (var i = 0; i < countOfFeatures; i++) {
-  //     var randomFeature;
-  //
-  //     do {
-  //       randomFeature = AD_FEATURES[getRandom(AD_FEATURES.length)];
-  //     } while (adRandomFeatures.indexOf(randomFeature) !== -1);
-  //
-  //     adRandomFeatures[i] = randomFeature;
-  //   }
-  //
-  //   return adRandomFeatures;
-  // };
 
   // массив объектов объявлений
   var getAdsList = function (adsArray) {
